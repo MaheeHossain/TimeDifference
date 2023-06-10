63f412cf99985e9ab02384ee63fb6c61b4ff6734
Mahee Hossain <mahee.hossain@student.unimelb.edu.au>
2023-06-10 12:43:28 +1000

    Expanded time difference

--- timeFunctions/getDifference.py
+++ timeFunctions/getDifference.py
@@ -1,23 +1,47 @@
 # Code written by Mahee Hossain: https://github.com/MaheeHossain
 # Gets the two times and gets the difference
 
-#25/04/1924 19:18:42.024
-#14/05/2005 20:15:23.425
-
-#import printTimes
 from timeFunctions.timeConstants import STRING_SPLITTERS
 import timeFunctions.timeConstants as timeConstants
 from timeFunctions.printTimes import printBothTimeArrays
+import datetime
 
 def differenceInTimes(times): 
     # Break the times into arrays with helper function splitDate
     # Day/Month/Year Hours:Minutes:Seconds.Microseconds
-    timeA = splitDate(times[0])
-    timeB = splitDate(times[1])
-    printBothTimeArrays(timeA, timeB)
+    timeA = turnArrayIntoDatetime(splitDate(times[0]))
+    timeB = turnArrayIntoDatetime(splitDate(times[1]))
+    #printBothTimeArrays(timeA, timeB)
+    print(timeA)
+    return orderThenDifferntiateTimes(timeA, timeB)
+
+def orderThenDifferntiateTimes(timeA, timeB):
+    # First order the times, then differentiate them
+    # Year
+    
     return 0
 
 def splitDate(time):
     for splitter in timeConstants.STRING_SPLITTERS:
         time = time.replace(splitter, " ")
     return time.split(' ')[timeConstants.DAY:timeConstants.MICROSECOND+1]
+
+def turnArrayIntoDatetime(time):
+    # Take the time array and turn it into a datetime
+    date = datetime.datetime(
+        int(time[timeConstants.YEAR]),
+        int(time[timeConstants.MONTH]),
+        int(time[timeConstants.DAY]),
+        int(time[timeConstants.HOUR]),
+        int(time[timeConstants.MINUTE]),
+        int(time[timeConstants.SECOND]),
+        microsecondMultiplier(time[timeConstants.MICROSECOND])
+    )
+    return date
+
+def microsecondMultiplier(microseconds):
+    # Depending on how many digits the decimal point is, 
+    # fit it to the datetime format
+    digits = len(microseconds)
+    multiplier = timeConstants.MICROSECOND_MULTIPLIER / (10 ** digits)
+    return int(int(microseconds) * multiplier)
